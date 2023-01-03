600c96920636c211e77cc2868b92ff9b131fa5c9
ktakada <ktakada@umich.edu>
2023-01-03 07:28:06 -0500

    Added task counter

--- main.js
+++ main.js
@@ -6,6 +6,20 @@ window.addEventListener('load', () => {
     const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
     const list_el = document.querySelector("#tasks");
+    const counter = document.querySelector("#completed-list")
+
+    //Counter information
+    const counter_el = document.createElement("div");
+    counter_el.classList.add("completed");
+
+    const counter_tracker = document.createElement("input");
+    counter_tracker.classList.add("input");
+    counter_tracker.type = "number";
+    counter_tracker.value = 0;
+    counter_tracker.setAttribute("readonly", "readonly");
+
+    counter_el.appendChild(counter_tracker);
+    counter.appendChild(counter_el);
 
     form.addEventListener('submit', (e) => {
         e.preventDefault(); //Prevents the refreshing the page with clicks
@@ -74,6 +88,8 @@ window.addEventListener('load', () => {
 
         task_delete_el.addEventListener('click', () => {
             list_el.removeChild(task_el);
+            counter_tracker.value++;
+            //Add incrementer here
         });
 
         //End of function some how add to the local storage to prevent deletion upon refresh
